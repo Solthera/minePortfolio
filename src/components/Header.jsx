d7ea1bb4132c0e210e1ff477d61ce0afa479add9
Solthera <farelfrr09@gmail.com>
2025-08-22 19:39:55 +0700

    Fix: Fix logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
+import logo from "../assets/img/logo2.png";
+
 export default function Header() {
     return (
         <div>
             <header className="header">
-                <div className="logo"><img src="./src/assets/img/logo2.png" alt="Logo Farel" /></div>
+                <div className="logo"><img src={logo} alt="Logo Farel" /></div>
                 <nav className="nav">
                     <a href="#home">Home</a>
                     <a href="#portfolio">Portfolio</a>
